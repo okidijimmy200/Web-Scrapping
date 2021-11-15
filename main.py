649221d3abe97a9b496a8a3ed589480d35dc9d3f
okidijimmy200 <okidijimmie@gmail.com>
2021-11-15 14:59:32 +0300

    commit before changing to twitter search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+import re 
+from selenium import webdriver
 
 headers_Get = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
@@ -17,32 +18,53 @@ def google(q):
     q = '+'.join(q.split())
     url = 'https://www.google.com/search?q=' + q + '&ie=utf-8&oe=utf-8'
     r = s.get(url, headers=headers_Get)
+    driver = webdriver.Chrome('/home/jimmy/NickSon/test webscrapp/chromedriver')
+    driver.implicitly_wait(10)
 
     soup = BeautifulSoup(r.text, "lxml")
     jobs = soup.find_all('div', class_= 'ZINbbc xpd O9g5cc uUPGi')
     
-    for job in jobs:
+    for index, job in enumerate(jobs):
         url = job.find('a')["href"] 
         test = url.split('url=')[-1]
         x = test.startswith("http")
-        # if x is True:
-        #     print(f'url: {test}')
+        if x is True:
+            new_url = test.split('/&')[:1]
+            str1 = ''.join(new_url)
+            print(f'url: {str1}')
 
-        facebook_url = job.find('a', href=re.compile(r'twitter\.com/'))
-           
-        
-        if facebook_url != None:
-            test_2 = facebook_url["href"].split('url=')[-1]
-            x_2 = test_2.startswith("http")
-            if x_2 is True:
-                # print(f'facebook url: {test_2}')
-                new_url = test_2.split('%')[:1]
-                new_url_1 = ", ".join(new_url)
-                print(", ".join(new_url))
-                html_text = requests.get(f'{new_url_1}').text
-                soup_1 = BeautifulSoup(html_text, 'lxml')
-                jobs_1 = soup_1.find_all('div')
-                print(jobs_1)
-    return
+        company = q
+        if index == 0:
+            facebook_url = job.find('a', href=re.compile(r'%s\.com/'%company))
+            
+            if facebook_url != None and index == 0:
+                test_2 = facebook_url["href"].split('url=')[-1]
+                x_2 = test_2.startswith("http")
+                if x_2 is True:
+                    new_url = test_2.split('/&')[:1]
+                    str1 = ''.join(new_url)
+                    print(str1)
+                    driver.get(str1)
+                    continue_link = driver.find_elements_by_tag_name('a')
+                    # print(continue_link)
+                    output = []
+                    for li in continue_link:
+                        attr = li.get_attribute('href')
+                        output.append(attr)
+                    # print(str(output))
+                    for pp in output:
+                        pattern = re.compile(r'about\.com/')
+                        match = pattern.match(str(pp))
+                        print(match)
+                        # if match:
+                        #     print(pp)
 
-result = google('walmart')
+                    # newlist = list(filter(pattern.match, str(output))) 
+                    # print(newlist)
+
+                    while(True):
+                        pass
+                    
+
+
+result = google('amazon')
