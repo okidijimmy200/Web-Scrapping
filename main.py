44ae4889a1395823effc77f9d7348d5d89ef0323
okidijimmy200 <okidijimmie@gmail.com>
2021-11-17 15:25:55 +0300

    created save_email func

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re 
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
 from urllib.error import URLError, HTTPError
 
 headers_Get = {
@@ -48,16 +49,21 @@ def list_all_urls(results):
         # return str1
         if x is True:
             # print(f'url: {str1}')
-            print(str1)
-            # return str1
+            # print(str1)
+            yield str1
 
 
 def twitter_company_search(results):
     '''function to search for company details on twitter'''
+
+    driver = webdriver.Chrome('/home/jimmy/NickSon/test webscrapp/chromedriver')
+    driver.implicitly_wait(10)
+
     for result in results:
         '''perform regex on urls to get twitter url'''
         twitter_url = result.find('a', href=re.compile(r'twitter\.com/'))
-            
+        
+        '''if iterated twitter_url is found'''    
         if twitter_url != None:
             test_2 = twitter_url["href"].split('url=')[-1]
             x_2 = test_2.startswith("http")
@@ -65,55 +71,63 @@ def twitter_company_search(results):
                 new_url = test_2.split('%')[:1]
                 str1 = ", ".join(new_url)
                 print(str1)
-                # driver.get(str1)
+                driver.get(str1)
+                players = driver.find_element(By.CSS_SELECTOR, '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(2) > div > div > div:nth-child(1) > div > div:nth-child(4) > div > a')
+                links = players.get_attribute('href')
+                print(links)
+                return links
 
 
             while(True):
                 pass
+def email_search(links):
+    '''search company homepage for email'''
+    url = links
+    driver = webdriver.Chrome('/home/jimmy/NickSon/test webscrapp/chromedriver')
+    driver.get(url)
+    # r = driver.page_source
+    # soup = BeautifulSoup(r, "lxml")
+    # print(soup)
 
-result = google_search('amazon')
+    elems = driver.find_elements_by_xpath("//a[@href]")
+    output = []
+    for elem in elems:
+        if 'contact-us' in elem.get_attribute("href") or 'contact' in elem.get_attribute("href"):
+                # print(elem.get_attribute("href"))
+                x =  elem.get_attribute("href")
+                output.append(x)
+    url_contact = output[0]
+    # print(output)
+
+    n = requests.get(url_contact, headers=headers_Get)
+
+    soup_2 = BeautifulSoup(n.text, "lxml")
+
+    for company_email in soup_2.find_all("a"):
+        if '@' in company_email.text:
+            comp_email = company_email.text
+            return comp_email
+
+    for company_email in soup_2.find_all("p"):
+        if '@' in company_email.text:
+            comp_email = company_email.text
+            return comp_email
+
+def save_email(comp_email):
+    '''save email into .txt file'''
+    with open("results.txt", "a+") as file_object:
+        file_object.seek(0)
+
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        file_object.write(comp_email)
+
+result = google_search('movit')
 result_2 = scrap_company(result)
 result_3 = list_all_urls(result_2)
-# result_4 = twitter_company_search(result_2)
-print(result_3)
-
-# def google(q):
-#     s = requests.Session()
-#     q = '+'.join(q.split())
-#     url = 'https://www.google.com/search?q=' + q + '&ie=utf-8&oe=utf-8'
-#     r = s.get(url, headers=headers_Get)
-#     driver = webdriver.Chrome('/home/jimmy/NickSon/test webscrapp/chromedriver')
-#     driver.implicitly_wait(10)
-
-#     soup = BeautifulSoup(r.text, "lxml")
-#     jobs = soup.find_all('div', class_= 'ZINbbc xpd O9g5cc uUPGi')
-    
-#     for job in jobs:
-#         url = job.find('a')["href"] 
-#         test = url.split('url=')[-1]
-#         x = test.startswith("http")
-#         if x is True:
-#             new_url = test.split('/&')[:1]
-#             str1 = ''.join(new_url)
-#             print(f'url: {str1}')
+result_4 = twitter_company_search(result_2)
+result_5 = email_search(result_4)
+result_6 = save_email(result_5)
 
 
-#         facebook_url = job.find('a', href=re.compile(r'twitter\.com/'))
-        
-#         if facebook_url != None:
-#             test_2 = facebook_url["href"].split('url=')[-1]
-#             x_2 = test_2.startswith("http")
-#             if x_2 is True:
-#                 new_url = test_2.split('%')[:1]
-#                 str1 = ", ".join(new_url)
-#                 print(str1)
-#                 driver.get(str1)
-
-
-#                 while(True):
-#                     pass
-                    
-#     return
-
-
-# result = google('amazon')
